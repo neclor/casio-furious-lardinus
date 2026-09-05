3e5d339ee992fb65a2af709e7b80b595596ae1e1
neclor <neclordev@gmail.com>
2026-09-05 17:52:05 +0200

    a

--- game/src/flgame/systems/physics_manager.py
+++ game/src/flgame/systems/physics_manager.py
@@ -1,14 +1,15 @@
-from gamekit.math.utils import clampf
-from gamekit.math.vectors.vector2 import Vector2
-
 def _detects(source, target):
     return source.collision_mask & target.collision_layer > 0
 
 class PhysicsManager:
-    __slots__ = ('context',)
+    __slots__ = ('context', '_collide_tiles')
 
     def __init__(self, context):
         self.context = context
+        import gc
+        from flgame.systems.physics_tiles import collide_tiles
+        self._collide_tiles = collide_tiles
+        gc.collect()
 
     def resolve(self, objects):
         for i, game_object in enumerate(objects):
@@ -16,7 +17,7 @@ class PhysicsManager:
                 continue
             for other in objects[i + 1:]:
                 self._collide_pair(game_object, other)
-            self._collide_tiles(game_object)
+            self._collide_tiles(game_object, self.context.level)
 
     def _collide_pair(self, a, b):
         detect_a = _detects(a, b)
@@ -46,36 +47,3 @@ class PhysicsManager:
         else:
             a.position += push / 2
             b.position -= push / 2
-
-    def _collide_tiles(self, game_object):
-        level = self.context.level
-        tile_size = level.tile_size
-        position = game_object.position
-        radius = game_object.radius
-        min_x = int(max(0, (position.x - radius) // tile_size.x))
-        max_x = int(min((position.x + radius) // tile_size.x, level.tile_map_size.x - 1))
-        min_y = int(max(0, (position.y - radius) // tile_size.y))
-        max_y = int(min((position.y + radius) // tile_size.y, level.tile_map_size.y - 1))
-        for ty in range(min_y, max_y + 1):
-            for tx in range(min_x, max_x + 1):
-                tile = level.tile_map[ty][tx]
-                if tile is None or not _detects(game_object, tile):
-                    continue
-                nearest = self._nearest_tile_point(position, tx, ty, tile_size)
-                vector = position - nearest
-                distance = vector.length()
-                overlap = radius - distance
-                if overlap <= 0:
-                    continue
-                game_object.on_collision(tile)
-                if not (game_object.collidable and tile.collidable):
-                    continue
-                if game_object.static or distance == 0:
-                    continue
-                game_object.position += vector.normalized() * overlap
-
-    @staticmethod
-    def _nearest_tile_point(position, tx, ty, tile_size):
-        left = tx * tile_size.x
-        top = ty * tile_size.y
-        return Vector2(clampf(position.x, left, left + tile_size.x), clampf(position.y, top, top + tile_size.y))
